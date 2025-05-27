70560b0389d0c6f8c3cb64dda11cb8cc0972c0e0
dom338 <domenicopetrillod@gmail.com>
2025-05-27 20:01:10 +0200

    ok

---
+++ Source/SpanishCourseUE5/Public/MySpanishCourseUE5Character.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SpanishCourseUE5/SpanishCourseUE5Character.h"
+#include "MySpanishCourseUE5Character.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPANISHCOURSEUE5_API AMySpanishCourseUE5Character : public ASpanishCourseUE5Character
+{
+	GENERATED_BODY()
+	
+};
